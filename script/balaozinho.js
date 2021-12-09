c9deec14db25ae34f7922f3be660133404fda181
alan7s <alan7@outlook.com>
2021-12-09 18:11:38 -0300

    Exercício 2: Selecionar marcação e preencher controles && Fix Exercício 1

--- script/balaozinho.js
+++ script/balaozinho.js
@@ -3,9 +3,9 @@ let imgMarcacaoNodeList = document.querySelectorAll('.marcacao');
 
 for (imgMarcacaoEl of imgMarcacaoNodeList) {
     imgMarcacaoEl.addEventListener('mouseover', (e) => {
-        balaozinhoEl.innerHTML = `<h2>${imgMarcacaoEl.dataset.titulo}</h2>
-        <p>${imgMarcacaoEl.dataset.conteudo}</p>`
-        balaozinhoEl.style.color = imgMarcacaoEl.dataset.cor;
+        balaozinhoEl.innerHTML = `<h2>${e.currentTarget.dataset.titulo}</h2>
+        <p>${e.currentTarget.dataset.conteudo}</p>`
+        balaozinhoEl.style.color = e.currentTarget.dataset.cor;
     });
 
     imgMarcacaoEl.addEventListener('mouseout', (e) => {
--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -6,3 +6,46 @@ checkbox.addEventListener('change', (e) => {
         marcacaoEl.classList.toggle('marcacoes-ocultas');
     }
 });
+
+for (marcacaoEl of marcacaoNodeList) {
+    marcacaoEl.addEventListener('click', (e) => {
+        let atualMarcacaoEl = document.querySelector('.marcacao.selecionada');
+        atualMarcacaoEl.classList.remove('selecionada');
+        atualMarcacaoEl = e.currentTarget;
+        atualMarcacaoEl.classList.add('selecionada');
+        atualizaControles(atualMarcacaoEl);
+    });
+}
+
+function atualizaControles(marcacaoSelecionadaEl) {
+    let xInput = document.querySelector('#x-da-marcacao');
+    xInput.value = parseInt(marcacaoSelecionadaEl.style.left);
+
+    let yInput = document.querySelector('#y-da-marcacao');
+    yInput.value = parseInt(marcacaoSelecionadaEl.style.top);
+
+    let widthInput = document.querySelector('#largura-da-marcacao');
+    widthInput.value = parseInt(marcacaoSelecionadaEl.style.width);
+
+    let heightInput = document.querySelector('#altura-da-marcacao');
+    heightInput.value = parseInt(marcacaoSelecionadaEl.style.height)
+
+    let titleInput = document.querySelector('#titulo-da-marcacao');
+    titleInput.value = marcacaoSelecionadaEl.dataset.titulo;
+
+    let contentInput = document.querySelector('#conteudo-da-marcacao');
+    contentInput.value = marcacaoSelecionadaEl.dataset.conteudo;
+
+    let colorInput = document.querySelector('#cor-da-marcacao');
+    colorInput.value = marcacaoSelecionadaEl.dataset.cor;
+
+    let formatoOvalInput = document.querySelector('input[value="formato-oval"]');
+    let formatoRetangularInput = document.querySelector('input[value="formato-retangular"]');
+    if(marcacaoSelecionadaEl.dataset.formato === 'formato-oval'){
+        formatoOvalInput.checked = true;
+        formatoRetangularInput.checked = false;
+    }else{
+        formatoOvalInput.checked = false;
+        formatoRetangularInput.checked = true;
+    }
+}
