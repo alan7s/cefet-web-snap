c4424a1e93f056f6e3676c832cb280d221985124
alan7s <alan7@outlook.com>
2021-12-09 21:07:08 -0300

    Desafio 1: Trocar a foto por outro arquivo

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -4,3 +4,19 @@ let fotoEl = document.querySelector('.foto-anotada > img');
 filtroDaFoto.addEventListener('change', (e) => {
     fotoEl.style.filter = filtroDaFoto.value;
 });
+
+let imgEl = document.querySelector('#imagem');
+imgEl.addEventListener('change', readImg);
+
+function readImg(e){
+    const file = e.target.files[0];
+    if(file.type && !file.type.startsWith('image/')){
+        console.log('File is not an image.', file.type, file);
+        return;
+    }
+    const reader = new FileReader();
+    reader.addEventListener('load', (e) => {
+        fotoEl.src = e.target.result;
+    });
+    reader.readAsDataURL(file);
+}
